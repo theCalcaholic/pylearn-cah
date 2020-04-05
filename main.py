af7a670d2931d6a7d0dfe511f764050630783f5d
MoeZarella1 <moritz.muck@t-online.de>
2020-04-05 19:19:19 +0200

    Implement simple console based version of CAH
    Co-authored-by: Tobias K <thecalcaholic@web.de>

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
 
 from cards import answers, questions
+from random import choice, sample
+
+#shopping_list = ["Aepfel", "Milch",
+#    "Zucker"]
+
+#print(shopping_list[2])
+#print(choice (shopping_list))
 
 
+# if __name__ == "__main__":
+#     print(answers)
+#     print(questions)
 
-if __name__ == "__main__":
-    print(answers)
-    print(questions)
+selection = 0
+answers_sample = []
+
+while selection <= len(answers_sample):
+    print(choice(questions))
+    answers_sample = sample(answers, 3)
+    #print(answers_sample)
+    # ["a", "b", "c"] -enumerate-> [(0, "a"), (1, "b"), (2, "c")]
+
+    for nr, a in enumerate(answers_sample): 
+        print( f"   {str(nr+1)} {a}")
+
+    selection = int(input("Selection: "))
+
+    if 0 < selection <= len(answers_sample):
+        print(answers_sample[selection-1])
